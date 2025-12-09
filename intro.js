487602ca60c15f62ce35469ec664266648663f2b
agrimachaturvedi <agrimachaturvedi23@gmail.com>
2025-12-09 03:55:09 +0000

    initial commit of intro js

---
+++ intro.js
@@ -0,0 +1,8 @@
+// intro.js
+
+// Basic introduction program
+console.log("Hello, this is my first JavaScript program using GitHub Codespaces!");
+console.log("Name: Agrima Chaturvedi");
+console.log("College: United College Of Engineering and Research");
+console.log("Course: B.Tech CSE (AIML)");
+console.log("Today I am learning JavaScript basics and GitHub workflow.");
